8a20f69b9c49302073180b443f3d00afe79ebaa2
Xavier Bussiere <xavier.bussiere@gmail.com>
2021-03-05 12:15:25 -0500

    ajouté quelques fonctions

--- main.py
+++ main.py
@@ -32,6 +32,22 @@ liste_nom = [
     {"nom": "mollet", "genre": "m", "rime": "ait"},
     {"nom": "palais", "genre": "m", "rime": "ait"},
     {"nom": "couplet", "genre": "m", "rime": "ait"},
+    {"nom": "amour", "genre": "f", "rime": "our"},
+    {"nom": "moere", "genre": "f", "rime": "our"},
+    {"nom": "tourd", "genre": "m", "rime": "our"},
+    {"nom": "bourre", "genre": "m", "rime": "our"},
+    {"nom": "rebours", "genre": "m", "rime": "our"},
+    {"nom": "atour", "genre": "m", "rime": "our"},
+    {"nom": "séjour", "genre": "m", "rime": "our"},
+    {"nom": "discours", "genre": "m", "rime": "our"},
+    {"nom": "parcours", "genre": "m", "rime": "our"},
+    {"nom": "retour", "genre": "m", "rime": "our"},
+    {"nom": "jour", "genre": "m", "rime": "our"},
+    {"nom": "cour", "genre": "f", "rime": "our"},
+    {"nom": "four", "genre": "m", "rime": "our"},
+    {"nom": "sourd", "genre": "m", "rime": "our"},
+    {"nom": "secours", "genre": "m", "rime": "our"},
+    {"nom": "concours", "genre": "m", "rime": "our"},
 ]
 
 liste_verbe = [
@@ -45,91 +61,79 @@ liste_verbe = [
     "boit",
     "plante",
 ]
-liste_determinant_feminin = ["une", "la", "ta"]
-liste_determinant_masculin = ["un", "le", "ton"]
-liste_adjectif_feminin = [
-    "adroite",
-    "difficile",
-    "dure",
-    "facile",
-    "lisse",
-    "maladroite",
-    "pointue",
-    "rugueuse",
-    "tordue",
-    "absente",
-    "assise",
-    "basse",
-    "couchée",
-    "haute",
-    "présente",
-    "blonde",
-    "brune",
-    "calme",
-    "curieuse",
-    "différente",
-    "douce",
-    "énervée",
-    "gentille",
-    "grande",
-    "handicapée",
-    "inséparable",
-    "jalouse",
-    "moyenne",
-    "muette",
-    "noire",
-    "nouvelle",
-    "petite",
-    "polie",
-    "propre",
-    "rousse",
-    "sage",
-    "sale",
-    "sérieuse",
-    "sourde",
-    "tranquille",
+
+liste_determinant = [
+    {"m":"un", "f":"une"},
+    {"m":"le", "f":"la"},
+    {"m":"ton", "f":"ta"},
+    {"m":"mon", "f":"ma"},
 ]
-liste_adjectif_masculin = [
-    "adroit",
-    "difficile",
-    "dur",
-    "facile",
-    "lisse",
-    "maladroit",
-    "pointu",
-    "rugueux",
-    "tordu",
-    "absent",
-    "assis",
-    "bas",
-    "couché",
-    "haut",
-    "présent",
-    "blond",
-    "brun",
-    "calme",
-    "curieux",
-    "différent",
-    "doux",
-    "énervé",
-    "gentil",
-    "grand",
-    "handicapé",
-    "inséparable",
-    "jaloux",
-    "moyen",
-    "muet",
-    "noir",
-    "nouveau",
-    "petit",
-    "poli",
-    "propre",
-    "roux",
-    "sage",
-    "sale",
-    "sérieux",
-    "sourd",
-    "tranquille",
+
+liste_adjectif = [
+    {"m":"oblitérateur", "f":"oblitératrice"},
+    {"m":"vérificatif", "f":"vérificative"},
+    {"m":"congratulatoire", "f":"congratulatoire"},
+    {"m":"énumérateur", "f":"énumératrice"},
+    {"m":"apollinarien", "f":"apollinarienne"},
+    {"m":"sardanapalesque", "f":"sardanapalesque"},
+    {"m":"transfigurateur", "f":"transfiguratrice"},
+    {"m":"émerillonné", "f":"émerillonnée"},
+    {"m":"vivificateur", "f":"vivificatrice"},
+    {"m":"résurrectionnel", "f":"résurrectionnelle"},
+    {"m":"vociférateur", "f":"vocifératrice"},
+    {"m":"caméléonesque", "f":"caméléonesque"},
+    {"m":"phraséologique", "f":"phraséologique"},
+    {"m":"localisateur", "f":"localisatrice"},
+    {"m":"glorificateurs", "f":"glorificatrice"},
+    {"m":"épigrammatique", "f":"épigrammatique"},
+    {"m":"tintinnabulant", "f":"tintinnabulante"},
+    {"m":"irréalisé", "f":"irréalisée"},
+    {"m":"inassouvissable", "f":"inassouvissable"},
+    {"m":"valétudinaire", "f":"valétudinaire"},
+    {"m":"méphistophélique", "f":"méphistophélique"},
+    {"m":"prévaricateur", "f":"prévaricatrice"},
+
+]
+
+liste_preposition = [
+    "à",
+    "durant",
+    "pendant",
+    "après",
+    "en",
+    "pour",
+    "avant",
+    "entre",
+    "près",
+    "avec",
+    "excepté",
+    "sans",
+    "chez",
+    "hormis",
+    "sauf",
+    "concernant",
+    "hors",
+    "selon",
+    "contre",
+    "jusque",
+    "sous",
+    "dans",
+    "malgré",
+    "suivant",
+    "de",
+    "moyennant",
+    "sur",
+    "depuis",
+    "outre",
+    "vers",
+    "derrière",
+    "par",
+    "voici",
+    "dès",
+    "parmi",
+    "voilà​",
+    "devant",
+    "vu",
 ]
 
 
@@ -154,15 +158,15 @@ def rime_AABB():
     deuxieme_A = random.choice(liste_nom_rimant_A)
     deuxieme_B = random.choice(liste_nom_rimant_B)
     structure_AABB = "{}\n{}\n{}\n{}\n".format(
-        groupe_nominal(premier_A),
-        groupe_nominal(deuxieme_A),
-        groupe_nominal(premier_B),
-        groupe_nominal(deuxieme_B),
+        groupe_verbal_avec_preposition(premier_A),
+        groupe_nominal_adjectif(deuxieme_A).capitalize(),
+        groupe_verbal_avec_preposition(premier_B),
+        groupe_nominal_adjectif(deuxieme_B).capitalize(),
     )
     return structure_AABB
 
 
-def groupe_nominal(dict_nom=random.choice(liste_nom)):
+def groupe_nominal(dict_nom=None):
     """Création d'un groupe nominal à partir d'une liste de dictionnaires contenant des noms (noyaux).
     On détermine le déterminant par la suite et si on ajoute un complément.
 
@@ -172,9 +176,11 @@ def groupe_nominal(dict_nom=random.choice(liste_nom)):
     Returns:
         Un groupe nominal.
     """
+    if dict_nom is None:
+        random.seed(random.randrange(99999999))
+        dict_nom = random.choice(liste_nom)
     noyau = dict_nom["nom"]
     determinant = nom_vers_determinant(dict_nom)
-
     determinant = verifier_mot_debute_voyelle(noyau, determinant)
 
     if determinant[-1] == "'":
@@ -184,6 +190,30 @@ def groupe_nominal(dict_nom=random.choice(liste_nom)):
         groupe_nominal = "{} {}".format(determinant, noyau)
     return groupe_nominal
 
+def groupe_nominal_adjectif(dict_nom=None):
+    """Création d'un groupe nominal à partir d'une liste de dictionnaires contenant des noms (noyaux).
+    On détermine le déterminant par la suite et si on ajoute un complément.
+
+    Args:
+        dict_nom: Un dictionnaire contenant un nom. Par défaut on choisit un dictionnaire au hasard dans la liste.
+
+    Returns:
+        Un groupe nominal.
+    """
+    if dict_nom is None:
+        random.seed(random.randrange(99999999))
+        dict_nom = random.choice(liste_nom)
+    noyau = dict_nom["nom"]
+    determinant = nom_vers_determinant(dict_nom)
+    expansion = nom_vers_adjectif(dict_nom)
+    determinant = verifier_mot_debute_voyelle(expansion, determinant)
+
+    if determinant[-1] == "'":
+        # Dernier caractère est un apostrophe donc on doit coller déterminant avec noyau
+        groupe_nominal_adjectif = "{}{} {}".format(determinant, expansion, noyau)
+    else:
+        groupe_nominal_adjectif = "{} {} {}".format(determinant, expansion, noyau)
+    return groupe_nominal_adjectif
 
 def groupe_verbal():
     """Création d'un groupe verbal
@@ -197,6 +227,22 @@ def groupe_verbal():
     groupe_verbal = "{} {} {}".format(pronom, groupe_pronom(), noyau)
     return groupe_verbal
 
+def groupe_verbal_avec_preposition(Gn=None):
+    """Création d'un groupe verbal avec expansion prepositionnel
+
+    Returns:
+        un groupe verbal + Gprep
+    """
+    noyau = random.choice(liste_verbe)
+    Gprep = groupe_prepositionnel()
+    if Gn is None:
+        Gn = groupe_nominal()
+    else:
+        Gn = groupe_nominal(Gn)
+    groupe_verbal = "{} {} {}".format(Gprep, noyau, Gn)
+    groupe_verbal = groupe_verbal.capitalize()
+    return groupe_verbal
+
 
 def groupe_pronom():
     """Création d'un pronom
@@ -208,6 +254,18 @@ def groupe_pronom():
     groupe_pronom = "{}, {},".format(pronom, groupe_nominal())
     return groupe_pronom
 
+def groupe_prepositionnel():
+    """Création d'un groupe prepositionnel
+
+    Returns:
+        Un groupe prépositionnel avec son expansion groupe du nom.
+    """
+    preposition = random.choice(liste_preposition)
+    gn = groupe_nominal()
+    groupe_prepositionnel = "{} {}".format(preposition, gn)
+    return groupe_prepositionnel
+
+
 
 def nom_vers_determinant(dict_nom):
     """Retourne un déterminant du même genre que le nom.
@@ -218,10 +276,10 @@ def nom_vers_determinant(dict_nom):
     Returns:
         Un déterminant du même genre que le nom passé en argument.
     """
-    if dict_nom["genre"] == "f":
-        determinant = random.choice(liste_determinant_feminin)
-    else:
-        determinant = random.choice(liste_determinant_masculin)
+    dict_determinant = random.choice(liste_determinant)
+    for k in dict_determinant:
+        if k == dict_nom["genre"]:
+            determinant = dict_determinant[k]
     return determinant
 
 
@@ -236,6 +294,8 @@ def verifier_mot_debute_voyelle(nom, determinant):
             determinant = "l'"
         if determinant == "ta":
             determinant == "ton"
+        if determinant == "ma":
+            determinant = "mon"
     return determinant
 
 
@@ -248,10 +308,10 @@ def nom_vers_adjectif(dict_nom):
     Returns:
         Un adjectif du même genre que le nom passé en argument.
     """
-    if dict_nom["genre"] == "f":
-        adjectif = random.choice(liste_adjectif_feminin)
-    else:
-        adjectif = random.choice(liste_adjectif_masculin)
+    dict_adjectif = random.choice(liste_adjectif)
+    for k in dict_adjectif:
+        if k == dict_nom["genre"]:
+            adjectif = dict_adjectif[k]
     return adjectif
 
 
